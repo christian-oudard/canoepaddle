2aaa47fe88c3792d788f9bb033ce2815416ba2b2
Christian Oudard <christian.oudard@gmail.com>
2013-06-14 13:15:31 -0700

    Added an example showing broken angled joints for arcs.

---
+++ examples/thick_trefoil.py
@@ -0,0 +1,30 @@
+from canoepaddle import Pen
+
+p = Pen()
+p.set_width(1.0)
+
+def trefoil(origin, radius, num_leaves, leaf_angle, step=1):
+    p.turn_to(90)
+    points = []
+    for i in range(num_leaves):
+        p.move_to(origin)
+        p.turn_right(360 / num_leaves)
+        p.move_forward(radius)
+        points.append(p.position)
+
+    p.move_to(points[0])
+    for i in range(num_leaves):
+        next_point = points[((i + 1) * step) % num_leaves]
+        p.turn_toward(origin)
+        p.turn_right(leaf_angle / 2)
+        p.arc_to(next_point)
+
+trefoil((0, 0), 8, 3, 110)
+
+p.paper.set_style('''
+    stroke: black;
+    stroke-width: 0.15;
+    stroke-linecap: round;
+    fill: red;
+''')
+print(p.paper.format_svg(thick=True))
