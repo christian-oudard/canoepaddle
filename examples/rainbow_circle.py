51fee630f0d6f51e7d5c0e47a3f5fe6c2e8f06de
Christian Oudard <christian.oudard@gmail.com>
2013-07-17 13:04:32 -0700

    Added rainbow_circle example.

---
+++ examples/rainbow_circle.py
@@ -0,0 +1,37 @@
+import math
+
+from canoepaddle import Pen
+from grapefruit import Color
+
+
+def draw(p):
+    radius = 4
+    num_colors = 36
+    angle_step = 360 / num_colors
+
+    p.set_width(1.0)
+    p.move_to((0, 0))
+    p.turn_to(90)
+    p.move_forward(radius)
+    p.turn_right(90)
+
+    angle = 60
+    for _ in range(num_colors):
+        l = 74
+        chroma = 0.4
+        a = chroma * math.sin(math.radians(angle))
+        b = chroma * math.cos(math.radians(angle))
+        color = Color.NewFromLab(l, a, b)
+        assert color.isLegal
+        p.set_color(color)
+
+        p.arc_right(
+            angle_step,
+            center=(0, 0),
+        )
+        angle += angle_step
+
+if __name__ == '__main__':
+    p = Pen()
+    draw(p)
+    print(p.paper.format_svg())
