0a54b9ff8dddf60985ddac07ee95f560ea256d0e
Christian Oudard <christian.oudard@gmail.com>
2013-08-04 04:34:57 -0700

    Update thick_trefoil example to use outlines.

--- examples/thick_trefoil.py
+++ examples/thick_trefoil.py
@@ -1,10 +1,7 @@
 from canoepaddle import Pen
 
-#TODO: Use red outlined in black.
-
 
 p = Pen()
-p.set_stroke_mode(1.0)
 
 
 def trefoil(origin, radius, num_leaves, leaf_angle, step=1):
@@ -23,6 +20,9 @@ def trefoil(origin, radius, num_leaves, leaf_angle, step=1):
         p.turn_right(leaf_angle / 2)
         p.arc_to(next_point)
 
+p.set_stroke_mode(1.0, '#a00')
+trefoil((0, 0), 8, 3, 110)
+p.set_outline_mode(1.0, 0.1, '#111')
 trefoil((0, 0), 8, 3, 110)
 
 print(p.paper.format_svg())
